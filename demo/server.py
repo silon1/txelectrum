3190d5b8fd2e729012d8644b56dbec5e1bb9ec75
Meir Klemfner <meirk251@outlook.com>
2022-06-27 09:44:21 +0300

    demo: better error and exit handling in server.py

--- demo/server.py
+++ demo/server.py
@@ -18,6 +18,13 @@ def main(args: argparse.ArgumentParser) -> None:
             read_list = client_socks + [server_sock]
             ready_to_read, ready_to_write, in_error = select.select(read_list, client_socks, client_socks)
 
+            for sock in in_error:
+                print(sock.getpeername(), "Client connection closed with error!")
+                client_socks.remove(sock)
+                ready_to_read.remove(sock)
+                ready_to_write.remove(sock)
+                sock.close()
+
             for sock in ready_to_read:
                 if sock is server_sock:
                     client_sock, client_address = server_sock.accept()
@@ -26,17 +33,12 @@ def main(args: argparse.ArgumentParser) -> None:
                     continue
 
                 sock_addr = sock.getpeername()
-
-                if sock in in_error:
-                    print(sock_addr, "Client connection closed with error!")
-                    client_socks.remove(sock)
-                    sock.close()
-                    continue
                 
                 msg_type, data = protocol.read(sock)
                 if msg_type == protocol.EXIT:
                     print(sock_addr, "Client connection closed!")
                     client_socks.remove(sock)
+                    ready_to_write.remove(sock)
                     sock.close()
                 elif msg_type == protocol.CREATE_KEYS:
                     print(sock_addr, "Creating new keys")
@@ -47,6 +49,7 @@ def main(args: argparse.ArgumentParser) -> None:
                 else:
                     print(sock_addr, "Unexpected Message Type")
                     client_socks.remove(sock)
+                    ready_to_write.remove(sock)
                     sock.close()
 
             for message in messages:
