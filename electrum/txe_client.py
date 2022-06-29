9df8803ca67c138b2fee35b494fdc4dea5630bbe
Meir Klemfner <meirk251@outlook.com>
2022-06-29 17:46:17 +0300

    txe_client: add design

---
+++ electrum/txe_client.py
@@ -0,0 +1,39 @@
+"""
+This module is a client that communicates with a TXE server
+in order to securely sign data using ECDSA secp256k1 algorithm.
+
+Usage:
+1. Create a new wallet:
+    pubkey = create_wallet(password)
+
+2. Sign a transaction:
+    try:
+        sig = sign(transaction, password, pubkey)
+        validate_sig(sig, pubkey) # Out of txe_client's responsibilities
+    except TxeWrongPasswordException:
+        # handle the exception
+"""
+
+def create_wallet(password:str) -> bytes:
+    """
+    Creates a new session with the TXE server
+    in order to create a new wallet.
+
+    Returns the public key of the new wallet.
+    """
+
+    pass
+
+
+def sign(data:bytes, password:str, pubkey:bytes) -> bytes:
+    """
+    Sends the data to be signed in the TXE using the ECDSA secp256k1 algorithm.
+    Returns the data's digital signature given by the TXE.
+    IMPORTANT: The digital signature is not validated.
+    """
+
+    pass
+
+
+class TxeWrongPasswordException(Exception):
+    pass
