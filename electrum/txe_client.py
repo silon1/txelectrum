c7ff3f6e9bef21c7907e9edb5702f270b77d7aa9
Meir Klemfner <meirk251@outlook.com>
2022-07-10 21:37:57 +0300

    Update txe_client design
    Removed the responsiblity of signing the digital signature out of this
    module. The reasons are the following:
    * I don't know how electrum handles ECDSA secp256k1 signature validation.
    * I don't develop the client with the plugin, so it will be hard to test
      it.

--- electrum/txe_client.py
+++ electrum/txe_client.py
@@ -2,8 +2,7 @@
 This module implements functions that communicate with a TXE server.
 By using this module an application can create a key pair and sign
 data using the ECDSA secp256k1 algorithm. Signing data is protected
-with a password. Each function creates a new socket to communicate
-with the server and closes it.
+with a password.
 
 Example Usage:
 * Create a new wallet:
@@ -15,22 +14,17 @@ Example Usage:
 * Sign a transaction:
     try:
         sig = sign(transaction, TXE_SHA256, password, pubkey)
+        validate_sig(sig) # Out of txe_client's responsibilities
     except TxeWrongPasswordError:
         # handle the exception
     except TxeException:
         # handle other exceptions
 """
 
+
 TXE_SHA1 = 0
 TXE_SHA256 = 1
 TXE_SHA512 = 2
-
-
-"""
-Number of times to request the server to sign again and validate the response
-when the first validation failed.
-"""
-__TXE_NUM_VALIDATIONS = 10
 
 
 def create_keypair(password:str) -> bytes:
@@ -50,8 +44,7 @@ def create_keypair(password:str) -> bytes:
 def sign(buffer:bytes, hash_func:int, password:str, pubkey:bytes) -> bytes:
     """
     Requests the server to sign the buffer with the pair of the public key
-    and a selected hash function. This function validates the digital signature
-    before returning it.
+    and a selected hash function.
 
     The parameters for this function are the following:
     1. buffer to be signed
@@ -64,14 +57,14 @@ def sign(buffer:bytes, hash_func:int, password:str, pubkey:bytes) -> bytes:
     * txe_client.TXE_SHA256
     * txe_client.TXE_SHA512
 
-    Returns the buffer's digital signature.
+    Returns the buffer's digital signature. This function doesn't validate
+    the digital signautre. The caller MUST validate the digital signature
+    for security.
 
     This function may throw the following exceptions:
     * TxeMissingPrivateKeyError when the server couldn't find the key pair
     of this public key.
     * TxeWrongPasswordError when the password is incorrect.
-    * TxeValidationError when this function failed to validate the digital
-    signature.
     * TxeConnectionError when the connection couldn't be made or is
     closed before receiving the signature.
     """
@@ -94,7 +87,4 @@ class TxeWrongPasswordError(TxeException):
 class TxeMissingPrivateKeyError(TxeException):
     pass
 
-
-class TxeValidationError(TxeException):
-    pass
 
