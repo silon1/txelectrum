a705eefcda3ce63376983b44cb22cd07ba010349
Meir Klemfner <meirk251@outlook.com>
2022-07-10 14:02:41 +0300

    Update the design of txe_client
    The following changes have been made:
    * Add option to select an hash function
    * Rewrite some of the comments
    * Add a base txe exception
    * Add more txe exceptions

--- electrum/txe_client.py
+++ electrum/txe_client.py
@@ -1,32 +1,40 @@
 """
 This module implements functions that communicate with a TXE server.
-By using this module an application can create a key-pair and sign
+By using this module an application can create a key pair and sign
 data using the ECDSA secp256k1 algorithm. Signing data is protected
 with a password. Each function creates a new socket to communicate
 with the server and closes it.
 
 Example Usage:
-1. Create a new wallet:
+* Create a new wallet:
     try:
         pubkey = create_keypair(password)
-    except TxeConnectionError:
+    except TxeException:
         # handle the exception
 
-2. Sign a transaction:
+* Sign a transaction:
     try:
-        sig = sign(transaction, password, pubkey)
-        validate_sig(sig, pubkey) # Out of txe_client's responsibilities
+        sig = sign(transaction, TXE_SHA256, password, pubkey)
     except TxeWrongPasswordError:
         # handle the exception
-    except TxeConnectionError:
-        # handle the exception
+    except TxeException:
+        # handle other exceptions
 """
+
+TXE_SHA1 = 0
+TXE_SHA256 = 1
+TXE_SHA512 = 2
+
+"""
+Number of times to request the server to sign again and validate the response
+when the first validation failed.
+"""
+__TXE_NUM_VALIDATIONS = 10
 
 def create_keypair(password:str) -> bytes:
     """
-    Creates a new session with the TXE server and requests to create
-    a new key-pair using the ECSDA secp256k1 algorithm. The private key
-    will be protected with the given password.
+    Requests the server to create a new key pair using the ECSDA secp256k1
+    algorithm. The private key will be protected with the given password.
 
     Returns the created public key.
 
@@ -37,25 +45,54 @@ def create_keypair(password:str) -> bytes:
     pass
 
 
-def sign(data:bytes, password:str, pubkey:bytes) -> bytes:
+def sign(buffer:bytes, hash_func:int, password:str, pubkey:bytes) -> bytes:
     """
-    Creates a new session with the TXE server and sends the data
-    for it to be signed with the private key.
+    Requests the server to sign the buffer with the pair of the public key
+    and a selected hash function. This function validates the digital signature
+    before returning it.
 
-    Returns the data's digital signature given by the TXE.
-    IMPORTANT: The digital signature is not validated.
+    The parameters for this function are the following:
+    1. buffer to be signed
+    2. hash function to use when signed
+    3. password to unlock the key pair
+    4. public key to identify the key pair to use
 
-    TxeWrongPasswordError will be thrown if the password is incorrect.
-    TxeConnectionError will be thrown if the connection couldn't be made,
-    or is closed before receiving the signature.
+    hash_func must be one of the following:
+    * txe_client.TXE_SHA1
+    * txe_client.TXE_SHA256
+    * txe_client.TXE_SHA512
+
+    Returns the buffer's digital signature.
+
+    This function may throw the following exceptions:
+    * TxeMissingPrivateKey when the server couldn't find the key pair of
+    this public key.
+    * TxeWrongPasswordError when the password is incorrect.
+    * TxeValidationError when this function failed to validate the digital
+    signature.
+    * TxeConnectionError when the connection couldn't be made or is
+    closed before receiving the signature.
     """
 
     pass
 
 
-class TxeWrongPasswordError(Exception):
+class TxeException(Exception):
     pass
 
 
-class TxeWrongPasswordError(Exception):
+class TxeConnectionError(TxeException):
     pass
+
+
+class TxeWrongPasswordError(TxeException):
+    pass
+
+
+class TxeMissingPrivateKey(TxeException):
+    pass
+
+
+class TxeValidationError(TxeException):
+    pass
+
