e89c21e3588a850b22f54c7eb3a840a38241c7fa
Meir Klemfner <meirk251@outlook.com>
2022-07-17 12:03:13 +0300

    Update txe_client.py design

--- electrum/txe_client.py
+++ electrum/txe_client.py
@@ -10,10 +10,9 @@ Example Usage:
         pubkey = create_keypair(password)
     except TxeException:
         # handle the exception
-
 * Sign a transaction:
     try:
-        sig = sign(transaction, TXE_SHA256, password, pubkey)
+        sig = sign(transaction, password, pubkey)
         validate_sig(sig) # Out of txe_client's responsibilities
     except TxeWrongPasswordError:
         # handle the exception
@@ -22,14 +21,9 @@ Example Usage:
 """
 
 
-TXE_SHA1 = 0
-TXE_SHA256 = 1
-TXE_SHA512 = 2
-
-
 def create_keypair(password:str) -> bytes:
     """
-    Requests the server to create a new key pair using the ECSDA secp256k1
+    Requests the server to create a new key pair using the secp256k1
     algorithm. The private key will be protected with the given password.
 
     Returns the created public key.
@@ -41,25 +35,19 @@ def create_keypair(password:str) -> bytes:
     pass
 
 
-def sign(buffer:bytes, hash_func:int, password:str, pubkey:bytes) -> bytes:
+def sign(buffer:bytes, password:str, pubkey:bytes) -> bytes:
     """
     Requests the server to sign the buffer with the pair of the public key
     and a selected hash function.
 
     The parameters for this function are the following:
-    1. buffer to be signed
-    2. hash function to use when signed
-    3. password to unlock the key pair
-    4. public key to identify the key pair to use
+    1. Buffer to be signed.
+    2. Password to unlock the key pair.
+    3. Public key to identify the key pair to use.
 
-    hash_func must be one of the following:
-    * txe_client.TXE_SHA1
-    * txe_client.TXE_SHA256
-    * txe_client.TXE_SHA512
-
-    Returns the buffer's digital signature. This function doesn't validate
-    the digital signautre. The caller MUST validate the digital signature
-    for security.
+    Returns the buffer's digital signature in DER format. This function
+    doesn't validate the digital signautre. The caller MUST validate the
+    digital signature for security.
 
     This function may raise the following exceptions:
     * TxeMissingPrivateKeyError when the server couldn't find the key pair
