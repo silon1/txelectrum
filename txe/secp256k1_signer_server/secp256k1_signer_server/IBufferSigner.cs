4c3235ff746e5bc96d222b7ec3fb1e6ffff2f267
Meir Klemfner <meirk251@outlook.com>
2022-07-18 12:41:43 +0300

    Implement server

---
+++ txe/secp256k1_signer_server/secp256k1_signer_server/IBufferSigner.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace secp256k1_signer_server
+{
+    public interface IBufferSigner
+    {
+        Task<byte[]> SignBuffer(byte[] hashedBuffer, byte[] password, byte[] publicKey);
+    }
+}
---
+++ txe/secp256k1_signer_server/secp256k1_signer_server/MissingPrivateKeyError.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace secp256k1_signer_server
+{
+    [Serializable]
+    internal class MissingPrivateKeyError : Exception
+    {
+        public MissingPrivateKeyError()
+        {
+        }
+
+        public MissingPrivateKeyError(string message) : base(message)
+        {
+        }
+
+        public MissingPrivateKeyError(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected MissingPrivateKeyError(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
