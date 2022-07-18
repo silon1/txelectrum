f708c9c0298317461207aca9c8cdf730d26f1331
Meir Klemfner <meirk251@outlook.com>
2022-07-18 13:57:26 +0300

    Implement AppletSigner and the boot strapper

---
+++ txe/secp256k1_signer_server/secp256k1_signer_server/AppletBootstrapper.cs
@@ -0,0 +1,22 @@
+﻿using Nancy;
+using Nancy.Bootstrapper;
+using Nancy.TinyIoc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace secp256k1_signer_server
+{
+    public class AppletBootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+        {
+            // The bootstrapper will call the signer's Dispose method when the server is being closed.
+            var signer = new AppletSigner();
+            container.Register<IKeyPairCreator, AppletSigner>(signer);
+            container.Register<IBufferSigner, AppletSigner>(signer);
+        }
+    }
+}
