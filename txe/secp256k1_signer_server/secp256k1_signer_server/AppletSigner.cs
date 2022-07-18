11b072e3cc265fa8796f1798b35d5613f84c51ba
Meir Klemfner <meirk251@outlook.com>
2022-07-18 13:57:26 +0300

    Implement AppletSigner and the boot strapper

---
+++ txe/secp256k1_signer_server/secp256k1_signer_server/AppletSigner.cs
@@ -0,0 +1,53 @@
+﻿using Intel.Dal;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace secp256k1_signer_server
+{
+    public class AppletSigner : IKeyPairCreator, IBufferSigner, IDisposable
+    {
+        private const string APPLET_ID = "e48e7440-4f22-4639-bae5-7216d720347e";
+
+        private Jhi m_jhi;
+        private JhiSession m_session;
+        private bool m_open;
+
+        public AppletSigner()
+        {
+            string appletPath = Path.GetFullPath("../../../../secp256k1_signer/bin/secp256k1_signer.dalp");
+            byte[] initBuffer = new byte[] { };
+
+            m_jhi = Jhi.Instance;
+            m_jhi.Install(APPLET_ID, appletPath);
+            m_jhi.CreateSession(APPLET_ID, JHI_SESSION_FLAGS.None, initBuffer, out m_session);
+            m_open = true;
+        }
+
+        public async Task<byte[]> CreateKeyPair(byte[] password)
+        {
+            return await new ExampleSigner().CreateKeyPair(password);
+        }
+
+        public async Task<byte[]> SignBuffer(byte[] hashedBuffer, byte[] password, byte[] publicKey)
+        {
+            return await new ExampleSigner().SignBuffer(hashedBuffer, password, publicKey);
+        }
+
+        public void Dispose()
+        {
+            // Handling multiple Dispose calls with m_open. AppletBootstrapper calls the
+            // Dispose twice, and I didn't find a way to avoid that.
+
+            if (m_open)
+            {
+                m_open = false;
+                m_jhi.CloseSession(m_session);
+                m_jhi.Uninstall(APPLET_ID);
+            }
+        }
+    }
+}
--- txe/secp256k1_signer_server/secp256k1_signer_server/Program.cs
+++ txe/secp256k1_signer_server/secp256k1_signer_server/Program.cs
@@ -28,6 +28,7 @@ namespace secp256k1_signer_server
             {
                 RewriteLocalhost = false
             };
+
             using (var host = new NancyHost(config, new Uri("http://localhost:51841")))
             {
                 host.Start();
@@ -35,48 +36,19 @@ namespace secp256k1_signer_server
                 Console.ReadLine();
             }
 
-            Jhi jhi = Jhi.Instance;
-            JhiSession session;
-
-            // This is the UUID of this Trusted Application (TA).
-            //The UUID is the same value as the applet.id field in the Intel(R) DAL Trusted Application manifest.
-            string appletID = "e48e7440-4f22-4639-bae5-7216d720347e";
-            // This is the path to the Intel Intel(R) DAL Trusted Application .dalp file that was created by the Intel(R) DAL Eclipse plug-in.
-            string appletPath = Path.GetFullPath("../../../../secp256k1_signer/bin/secp256k1_signer.dalp");
-
-
-            // Install the Trusted Application
-            Console.WriteLine("Installing the applet.");
-            jhi.Install(appletID, appletPath);
-
-            // Start a session with the Trusted Application
-            byte[] initBuffer = new byte[] { }; // Data to send to the applet onInit function
-            Console.WriteLine("Opening a session.");
-            jhi.CreateSession(appletID, JHI_SESSION_FLAGS.None, initBuffer, out session);
-
             // Send and Receive data to/from the Trusted Application
-            byte[] sendBuff = Encoding.UTF8.GetBytes("Signature of ECDSA SECP256k1"); // A message to send to the TA
-            int responseCode; // The return value that the TA provides using the IntelApplet.setResponseCode method
-            for (int command = 0; command < 2; ++command)
-            {
-                byte[] recvBuff = new byte[2000]; // A buffer to hold the output data from the TA
-                Console.WriteLine("Performing send and receive operation with command " + command);
-                jhi.SendAndRecv2(session, command, sendBuff, ref recvBuff, out responseCode);
-                Console.WriteLine("Response buffer is " + BitConverter.ToString(recvBuff).Replace("-", string.Empty));
-                Console.WriteLine("Response buffer length is " + recvBuff.Length);
-                Console.WriteLine("Response code is " + responseCode);
-            }
+            //byte[] sendBuff = Encoding.UTF8.GetBytes("Signature of ECDSA SECP256k1"); // A message to send to the TA
+            //int responseCode; // The return value that the TA provides using the IntelApplet.setResponseCode method
+            //for (int command = 0; command < 2; ++command)
+            //{
+            //    byte[] recvBuff = new byte[2000]; // A buffer to hold the output data from the TA
+            //    Console.WriteLine("Performing send and receive operation with command " + command);
+            //    jhi.SendAndRecv2(session, command, sendBuff, ref recvBuff, out responseCode);
+            //    Console.WriteLine("Response buffer is " + BitConverter.ToString(recvBuff).Replace("-", string.Empty));
+            //    Console.WriteLine("Response buffer length is " + recvBuff.Length);
+            //    Console.WriteLine("Response code is " + responseCode);
+            //}
 
-            // Close the session
-            Console.WriteLine("Closing the session.");
-            jhi.CloseSession(session);
-
-            // Uninstall the Trusted Application
-            Console.WriteLine("Uninstalling the applet.");
-            jhi.Uninstall(appletID);
-
-            Console.WriteLine("Press Enter to finish.");
-            Console.Read();
         }
     }
 }
