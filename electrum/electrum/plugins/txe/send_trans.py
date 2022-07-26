291fae7906c42cc75b928e2744949da3e29925c5
a <6g6A07tAE@protonmail.com>
2022-07-26 16:00:09 +0300

    delete not necessary PubKeyNotFound rais

--- electrum/electrum/plugins/txe/send_trans.py
+++ electrum/electrum/plugins/txe/send_trans.py
@@ -8,12 +8,21 @@ class Pwd:
     p = None
 
 
-def send_trans(tx):
-    # c = Commands(config=Pwd.main_window.config,
-    #              daemon=Pwd.main_window.gui_object.daemon,
-    #              network=Pwd.main_window.network)
-    # res = c.broadcast(tx)
+def lendian(x):
+    return ''.join(textwrap.wrap(x, 2)[::-1])
 
+
+def ith(i, little=False, zf=0):
+    x = '0' * (len(s := hex(i)[2:]) % 2) + s
+    x = x.zfill(zf) if zf else x
+    return lendian(x) if little else x
+
+
+def lth(s, zf=2):
+    return ith(int(len(s) // 2), True, zf)
+
+
+def send_trans(tx):
     try:
         p = '\\'.join(os.path.abspath(__file__).split('\\')[:-1])
         print(tx)
@@ -37,6 +46,15 @@ class TX:
         self.txins = txins
         self.txouts = txouts
         self.lock_time = ith(lock_time, True, TX.locktime_zf)
+
+    @staticmethod
+    def from_tx(tx: PartialTransaction):
+        tx = tx.to_json()
+        version = tx['version']
+        txins = tx['inputs']
+        txouts = tx['outputs']
+        locktime = tx['locktime']
+        return TX(version, txins, txouts, locktime)
 
     def _in(self):
         utxo = ith(len(self.txins))
@@ -62,28 +80,5 @@ class TX:
             s += o['scriptpubkey']
         return s
 
-    @staticmethod
-    def from_tx(tx: PartialTransaction):
-        tx = tx.to_json()
-        version = tx['version']
-        txins = tx['inputs']
-        txouts = tx['outputs']
-        locktime = tx['locktime']
-        return TX(version, txins, txouts, locktime)
-
     def create(self):
         return self.version + self._in() + self._out() + self.lock_time
-
-
-def lendian(x):
-    return ''.join(textwrap.wrap(x, 2)[::-1])
-
-
-def ith(i, little=False, zf=0):
-    x = '0' * (len(s := hex(i)[2:]) % 2) + s
-    x = x.zfill(zf) if zf else x
-    return lendian(x) if little else x
-
-
-def lth(s, zf=2):
-    return ith(int(len(s) // 2), True, zf)
